7f8cae92024ddc3fc925ed0d169b12d60ab765f7
stefin <stefinracho@gmail.com>
2026-01-09 12:24:03 -0800

    fix(testing-suite): separate unit and e2e testing commands
    Executing `npm test` failed because Vitest tried to execute Playwright
    tests, requiring separating of `npm run ...` commands.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 import { WxtVitest } from "wxt/testing/vitest-plugin";
 
 export default defineConfig({
   plugins: [WxtVitest()],
   test: {
     environment: "happy-dom",
+    exclude: [...configDefaults.exclude, "tests/e2e/**"],
   },
 });
